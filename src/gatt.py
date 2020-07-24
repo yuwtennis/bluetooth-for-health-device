c64e683b0f218abface372bb0e9b5984a2179134
ywatanabe <yu.w.tennis@gmail.com>
2020-07-24 18:00:54 +0900

    Fixed html.

--- src/gatt.py
+++ src/gatt.py
@@ -2,9 +2,26 @@ import pygatt
 
 class Gatt:
 
+    """Fetches rows from a Smalltable.
+    """
+
     def __init__(self):
         self._adapter = pygatt.GATTToolBackend()
 
+    """Fetches rows from a Smalltable.
+
+    Retrieves rows pertaining to the given keys from the Table instance
+    represented by table_handle.  String keys will be UTF-8 encoded.
+
+    Args:
+        table_handle: An open smalltable.Table instance.
+
+    Returns:
+        A dict mapping keys to the corresponding table row data
+
+    Raises:
+        IOError: An error occurred accessing the smalltable.
+    """
     def start(self):
         """Start module """
         self._adapter.start()
